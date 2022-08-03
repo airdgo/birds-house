4fc7503332904d45a267d3e6a56ad7e6b6c881f2
Vlad Dragoi <vladdragoi1@gmail.com>
2022-08-03 12:08:30 +0300

    responsive styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Layout from "../layout/Layout";
-import { Landing } from "../sections";
+import { Landing, HomeBrake, Rooms } from "../sections";
 
 export default function Index() {
 	return (
 		<Layout>
 			<Landing />
+			<HomeBrake />
+			<Rooms />
 		</Layout>
 	);
 }
