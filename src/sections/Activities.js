8873c70c3395b4780618b61984b123a51850a998
Vlad Dragoi <vladdragoi1@gmail.com>
2022-08-13 11:42:25 +0300

    carousel

--- src/sections/Activities.js
+++ src/sections/Activities.js
@@ -1,3 +1,15 @@
+import { Carousel } from "../components/Carousel";
+import Image from "next/image";
+import { roomsImages } from "../constants";
+
 export const Activities = () => {
-	return <section className="h-screen bg-primary">Activities</section>;
+	return (
+		<section className="h-screen bg-primary">
+			<Carousel show={3} infiniteLoop={true}>
+				{roomsImages.map((item) => (
+					<Image src={item.src} alt={item.alt} />
+				))}
+			</Carousel>
+		</section>
+	);
 };
