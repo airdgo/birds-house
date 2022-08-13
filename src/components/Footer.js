bdc2e15402e106f4ffd1cefad4273a6f698f1fc8
Vlad Dragoi <vladdragoi1@gmail.com>
2022-08-13 17:00:15 +0300

    desktop layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,35 @@
+import Image from "next/image";
+import { roomsImages } from "../constants";
+
 export const Footer = () => {
 	return (
-		<footer>
-			{/* some code */}
-			{/* some more code */}
+		<footer className="grid h-screen place-items-center bg-secondary text-gray-400">
+			<div className="grid grid-cols-2 gap-4">
+				<h1 className="col-span-2">Location</h1>
+
+				<div>
+					<address className="not-italic">
+						Strada Alpina nr. 1, Petroșani, România
+					</address>
+				</div>
+
+				<div className="text-center">
+					<p>Instagram / Facebook</p>
+					<div className="grid grid-cols-3 gap-2">
+						{roomsImages.slice(0, 6).map((item) => (
+							<div className="relative aspect-square min-h-[10rem]">
+								<Image
+									src={item.src}
+									alt={item.alt}
+									layout="fill"
+									objectFit="cover"
+									objectPosition="center"
+								/>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
 		</footer>
 	);
 };
