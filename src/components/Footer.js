829e04dd5b4bfb8389fd7d87f8f2f11c82e42633
Vlad Dragoi <vladdragoi1@gmail.com>
2022-08-15 18:12:46 +0300

    google mapst functionality

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
 import Image from "next/image";
 import { roomsImages } from "../constants";
+import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
+import { useMemo } from "react";
 
 export const Footer = () => {
+	const { isLoaded } = useLoadScript({
+		googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+	});
+
 	return (
 		<footer className="grid h-screen place-items-center bg-secondary text-gray-400">
 			<div className="grid grid-cols-2 gap-4">
@@ -11,6 +17,7 @@ export const Footer = () => {
 					<address className="not-italic">
 						Strada Alpina nr. 1, Petroșani, România
 					</address>
+					{isLoaded ? <Map /> : <div>Loading...</div>}
 				</div>
 
 				<div className="text-center">
@@ -33,3 +40,22 @@ export const Footer = () => {
 		</footer>
 	);
 };
+
+function Map() {
+	const birdsHouseLoc = { lat: 45.38926207900881, lng: 23.459836545225667 };
+	const center = useMemo(() => birdsHouseLoc, []);
+	return (
+		<GoogleMap
+			zoom={10}
+			center={center}
+			mapContainerClassName="w-full h-[20rem]"
+			options={{
+				streetViewControl: false,
+				mapTypeControl: false,
+				fullscreenControl: false,
+			}}
+		>
+			<MarkerF position={birdsHouseLoc} />
+		</GoogleMap>
+	);
+}
