39243521c5e9f6ae27b6be1e30a43ee78e4ec001
Vlad Dragoi <vladdragoi1@gmail.com>
2022-08-19 12:53:09 +0300

    desktop styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,17 +12,32 @@ export const Footer = () => {
 		<footer className="grid min-h-screen place-items-center bg-secondary text-lg text-gray-600">
 			<div className="my-20 w-[90%] max-w-[90rem]">
 				<h1 className="mb-8 w-full text-3xl font-bold">Location</h1>
-				<div className="grid grid-cols-1 gap-8">
-					<div>
-						<address className="mb-7 not-italic">
-							Strada Alpina nr. 1, Petroșani, România
-						</address>
-						{isLoaded ? <Map /> : <div>Loading...</div>}
+
+				<div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+					<address className="not-italic">
+						Strada Alpina nr. 1, Petroșani, România
+					</address>
+
+					<div className="row-start-3 text-center md:row-start-auto">
+						<a
+							href="https://instagram.com/birdshouse__?igshid=YmMyMTA2M2Y="
+							target="_blank"
+						>
+							Instagram
+						</a>
+						<span className="mx-2">/</span>
+						<a
+							href="https://www.facebook.com/birdshouse_parang-102485455211263"
+							target="_blank"
+						>
+							Facebook
+						</a>
 					</div>
 
-					<div className="h-full text-center">
-						<p className="mb-7">Instagram / Facebook</p>
-						<div className="grid grid-cols-2 gap-2">
+					{isLoaded ? <Map /> : <div>Loading...</div>}
+
+					<div className="h-full w-full text-center">
+						<div className="grid grid-cols-2 gap-2 sm:grid-cols-3">
 							{roomsImages.slice(0, 6).map((item) => (
 								<div className="relative aspect-square min-h-[1rem]">
 									<Image
@@ -49,7 +64,7 @@ function Map() {
 		<GoogleMap
 			zoom={10}
 			center={center}
-			mapContainerClassName="w-full min-h-[14rem] aspect-video"
+			mapContainerClassName="min-h-[14rem] w-full md:min-h-full"
 			options={{
 				streetViewControl: false,
 				mapTypeControl: false,
