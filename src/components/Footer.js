ca01f4e5d479ccbb27daed28b2fc26674ffad164
Vlad Dragoi <vladdragoi1@gmail.com>
2022-08-19 15:15:56 +0300

    footer functionality

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 export const Footer = () => {
 	return (
 		<footer className="flex w-full items-center justify-center bg-[#f8f8f8] text-base font-light text-gray-400">
 			<ul className="my-16 flex gap-6">
-				<li>Contact</li>
-				<li>Book Now</li>
+				<li>
+					<Link href="/contact">Contact</Link>
+				</li>
+				<li>
+					<Link href="/book-now">Book Now</Link>
+				</li>
 			</ul>
 		</footer>
 	);
