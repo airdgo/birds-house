0c49e200055c0260dc7ec5f3dc80ff1571ade0bd
Vlad Dragoi <vladdragoi1@gmail.com>
2022-08-19 12:58:50 +0300

    extract map in separate component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { roomsImages } from "../constants";
-import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
-import { useMemo } from "react";
+import { useLoadScript } from "@react-google-maps/api";
+import { Map } from "../components";
 
 export const Footer = () => {
 	const { isLoaded } = useLoadScript({
@@ -56,22 +56,3 @@ export const Footer = () => {
 		</footer>
 	);
 };
-
-function Map() {
-	const birdsHouseLoc = { lat: 45.38926207900881, lng: 23.459836545225667 };
-	const center = useMemo(() => birdsHouseLoc, []);
-	return (
-		<GoogleMap
-			zoom={10}
-			center={center}
-			mapContainerClassName="min-h-[14rem] w-full md:min-h-full"
-			options={{
-				streetViewControl: false,
-				mapTypeControl: false,
-				fullscreenControl: false,
-			}}
-		>
-			<MarkerF position={birdsHouseLoc} />
-		</GoogleMap>
-	);
-}
