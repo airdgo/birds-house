70060bb0062857c212f15d180f20b6dea0abb3a7
Vlad Dragoi <vladdragoi1@gmail.com>
2022-08-21 12:30:27 +0300

    refactoring

--- src/sections/Rooms.js
+++ src/sections/Rooms.js
@@ -23,17 +23,16 @@ export const Rooms = () => {
 	);
 
 	return (
-		<section className="min-h-screen bg-neutral-50 py-12 font-primary text-neutral-500">
+		<section className="min-h-screen bg-neutral-50 py-12 font-primary text-neutral-600">
 			<div className="mx-auto h-full w-[90%] max-w-[90rem]">
 				<div>
 					<h1 className="text-center text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
 						Take a peek inside our cozy cabin
 					</h1>
 				</div>
-				<div className="container">
-					<div className="mt-16 grid grid-rows-3 gap-4 sm:grid-cols-2 md:grid-cols-4">
-						{renderImages}
-					</div>
+
+				<div className="mt-16 grid grid-rows-3 gap-4 sm:grid-cols-2 md:grid-cols-4">
+					{renderImages}
 				</div>
 
 				<Modal
