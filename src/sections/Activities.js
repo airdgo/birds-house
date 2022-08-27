b04fc9948ec4c8660b79f70039962b064a9e8a75
Vlad Dragoi <vladdragoi1@gmail.com>
2022-08-27 18:24:22 +0300

    remove previous carousel

--- src/sections/Activities.js
+++ src/sections/Activities.js
@@ -1,15 +1,3 @@
-import { Carousel } from "../components/Carousel";
-import Image from "next/image";
-import { roomsImages } from "../constants";
-
 export const Activities = () => {
-	return (
-		<section id="activities" className="h-screen bg-primary">
-			<Carousel show={3} infiniteLoop={true}>
-				{roomsImages.map((item) => (
-					<Image key={item.id} src={item.src} alt={item.alt} />
-				))}
-			</Carousel>
-		</section>
-	);
+	return <section id="activities" className="h-screen bg-primary"></section>;
 };
