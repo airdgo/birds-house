ac338be73847aa679d725cbdb3c794053e6cde3b
Vlad Dragoi <vladdragoi1@gmail.com>
2022-08-28 14:00:26 +0300

    add image

--- src/sections/Activities.js
+++ src/sections/Activities.js
@@ -1,21 +1,32 @@
 import { useRef } from "react";
 import { useDragToScroll } from "../hooks";
+import Image from "next/image";
+import backgroundImage from "../assets/home.jpeg";
 
 export const Activities = () => {
 	const ref = useRef();
-
 	const onMouseDown = useDragToScroll(ref);
 
 	return (
-		<section id="activities" className="min-h-screen bg-primary">
+		<section
+			id="activities"
+			className="grid min-h-screen place-items-center bg-primary"
+		>
 			<div
 				ref={ref}
 				onMouseDown={onMouseDown}
-				className="w-full cursor-grab overflow-auto whitespace-nowrap"
+				className="flex w-full overflow-auto"
 			>
-				<div className=" inline-block h-[30rem] w-[50rem] bg-red-400"></div>
-				<div className=" inline-block h-[30rem] w-[50rem] bg-yellow-400"></div>
-				<div className=" inline-block h-[30rem] w-[50rem] bg-green-400"></div>
+				<div className="relative h-[30rem] w-[50rem] shrink-0 overflow-hidden rounded-3xl">
+					<Image
+						src={backgroundImage}
+						layout="fill"
+						objectFit="cover"
+						objectPosition="center"
+					/>
+				</div>
+				<div className="h-[30rem] w-[50rem] shrink-0 bg-yellow-400"></div>
+				<div className="h-[30rem] w-[50rem] shrink-0 bg-green-400"></div>
 			</div>
 		</section>
 	);
