f6e88da8b10980d5d9592f588924f815c9c3447a
Vlad Dragoi <vladdragoi1@gmail.com>
2022-08-28 13:36:16 +0300

    add useDragToScroll hook

--- src/sections/Activities.js
+++ src/sections/Activities.js
@@ -1,3 +1,22 @@
+import { useRef } from "react";
+import { useDragToScroll } from "../hooks";
+
 export const Activities = () => {
-	return <section id="activities" className="h-screen bg-primary"></section>;
+	const ref = useRef();
+
+	const onMouseDown = useDragToScroll(ref);
+
+	return (
+		<section id="activities" className="min-h-screen bg-primary">
+			<div
+				ref={ref}
+				onMouseDown={onMouseDown}
+				className="w-full cursor-grab overflow-auto whitespace-nowrap"
+			>
+				<div className=" inline-block h-[30rem] w-[50rem] bg-red-400"></div>
+				<div className=" inline-block h-[30rem] w-[50rem] bg-yellow-400"></div>
+				<div className=" inline-block h-[30rem] w-[50rem] bg-green-400"></div>
+			</div>
+		</section>
+	);
 };
