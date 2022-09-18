9575f8f02da60c57f75928c235ee7fabae31a350
Vlad Dragoi <vladdragoi1@gmail.com>
2022-09-18 14:34:14 +0300

    change activities brake image

--- src/sections/ActivitiesBrake.js
+++ src/sections/ActivitiesBrake.js
@@ -1,4 +1,4 @@
-import activitiesBrake from "../assets/activitiesBrake.jpg";
+import activitiesBrake from "../assets/activitiesBrake2.jpeg";
 import { ParallaxBanner } from "react-scroll-parallax";
 
 export const ActivitiesBrake = () => {
@@ -8,6 +8,7 @@ export const ActivitiesBrake = () => {
 				layers={[{ image: activitiesBrake.src, speed: -15 }]}
 				className="aspect-[2/1]"
 			/>
+			<div className="absolute inset-0 aspect-[2/1] w-full bg-primary opacity-30" />
 			<div className="absolute inset-0 flex items-center justify-center">
 				<h1 className="w-[90%] max-w-6xl text-3xl font-thin sm:text-6xl md:text-7xl xl:text-8xl">
 					There's a whole world out there.
