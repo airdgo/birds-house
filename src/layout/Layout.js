b04a5620abe618d48704f6bd772f4115762b1630
Vlad Dragoi <vladdragoi1@gmail.com>
2022-11-06 10:46:30 +0200

    refactor: use pin prop instead of position sticky

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,7 +2,7 @@ import { Header, Footer } from "../components";
 
 export default function Layout({ children }) {
 	return (
-		<div className="overflow-clip scroll-smooth bg-primary font-primary">
+		<div className="scroll-smooth font-primary">
 			<Header />
 			<main>{children}</main>
 			<Footer />
