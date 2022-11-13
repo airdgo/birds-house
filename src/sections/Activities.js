ebfcaef85110206ee2e9d740e331d1a547d52fdb
Vlad Dragoi <vladdragoi1@gmail.com>
2022-11-13 15:38:03 +0200

    feat: use gasp to animate horizontally

--- src/sections/Activities.js
+++ src/sections/Activities.js
@@ -1,70 +1,77 @@
 import Image from "next/image";
 import { Carousel } from "../components";
 import { carouselImages, activitiesImages } from "../constants";
-import { ScrollTrigger, Tween } from "react-gsap";
-import { useState } from "react";
+import { gsap } from "gsap";
+import { useState, useEffect } from "react";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 export const Activities = () => {
 	const [isOpen, setIsOpen] = useState(false);
 	const [imageIndex, setImageIndex] = useState(0);
+	gsap.registerPlugin(ScrollTrigger);
+
+	useEffect(() => {
+		let ctx = gsap.context(() => {
+			gsap.to(".element", {
+				x: "-100%",
+				scrollTrigger: {
+					trigger: ".section",
+					markers: true,
+					scrub: 0.5,
+					end: "100%",
+					pin: true,
+					anticipatePin: 1,
+				},
+			});
+		});
+		return () => ctx.revert();
+	}, []);
 
 	return (
-		<section
-			id="activities"
-			className="relative min-h-[200vh] overflow-auto bg-primary py-[20vw]"
-		>
-			<ScrollTrigger
-				start="center center"
-				end="200% center"
-				scrub={0.5}
-				pin={true}
-				markers
+		<>
+			<section
+				id="activities"
+				className="section relative h-screen overflow-hidden bg-primary"
 			>
-				<Tween
-					to={{
-						x: "-100vw",
-					}}
-				>
-					<div className="flex h-full w-full flex-col items-center overflow-hidden md:flex-row md:overflow-visible first:[&>div]:rotate-12 md:first:[&>div]:rotate-0 [&:nth-child(3)]:[&>div]:rotate-flip-12 md:[&:nth-child(3)]:[&>div]:rotate-0 [&:nth-child(5)]:[&>div]:-rotate-flip-12 md:[&:nth-child(5)]:[&>div]:rotate-0 last:[&>div]:[&>span]:mb-7">
-						{carouselImages.map((image) => {
-							const isImageIdEven = image.id % 2 === 0;
+				<div className="element absolute top-1/2 left-1/2 flex h-full w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center overflow-hidden md:flex-row md:overflow-visible first:[&>div]:rotate-12 md:first:[&>div]:rotate-0 [&:nth-child(3)]:[&>div]:rotate-flip-12 md:[&:nth-child(3)]:[&>div]:rotate-0 [&:nth-child(5)]:[&>div]:-rotate-flip-12 md:[&:nth-child(5)]:[&>div]:rotate-0 last:[&>div]:[&>span]:mb-7">
+					{carouselImages.map((image) => {
+						const isImageIdEven = image.id % 2 === 0;
 
-							return isImageIdEven ? (
-								<div
-									key={image.id}
-									className="z-10 flex h-[32vw] min-h-[10rem] w-[50rem] shrink-0 justify-center overflow-hidden rounded-3xl md:h-[30vw] md:w-[20vw]"
-								>
-									<div className="relative flex w-full flex-col justify-center gap-[2px]">
-										<Image src={image.src} alt={image.alt} />
-										<hr className="border border-secondary" />
-									</div>
+						return isImageIdEven ? (
+							<div
+								key={image.id}
+								className="z-10 flex h-[32vw] min-h-[10rem] w-[50rem] shrink-0 justify-center overflow-hidden rounded-3xl md:h-[30vw] md:w-[20vw]"
+							>
+								<div className="relative flex w-full flex-col justify-center gap-[2px]">
+									<Image src={image.src} alt={image.alt} />
+									<hr className="border border-secondary" />
 								</div>
-							) : (
+							</div>
+						) : (
+							<div
+								key={image.id}
+								className="relative z-10 h-[50vw] min-h-[15rem] w-full shrink-0 md:h-[30vw] md:w-[40vw]"
+							>
 								<div
-									key={image.id}
-									className="relative z-10 h-[50vw] min-h-[15rem] w-full shrink-0 md:h-[30vw] md:w-[40vw]"
+									onClick={() => {
+										setImageIndex(image.id - 1);
+										setIsOpen(true);
+									}}
+									className="relative mx-auto h-full w-[90%] cursor-pointer overflow-hidden rounded-xl md:w-full md:rounded-3xl"
 								>
-									<div
-										onClick={() => {
-											setImageIndex(image.id - 1);
-											setIsOpen(true);
-										}}
-										className="relative mx-auto h-full w-[90%] cursor-pointer overflow-hidden rounded-xl md:w-full md:rounded-3xl"
-									>
-										<Image
-											src={image.src}
-											alt={image.alt}
-											layout="fill"
-											objectFit="cover"
-											objectPosition="center"
-										/>
-									</div>
+									<Image
+										src={image.src}
+										alt={image.alt}
+										layout="fill"
+										objectFit="cover"
+										objectPosition="center"
+									/>
 								</div>
-							);
-						})}
-					</div>
-				</Tween>
-			</ScrollTrigger>
+							</div>
+						);
+					})}
+				</div>
+			</section>
 			<Carousel
 				isOpen={isOpen}
 				onClose={() => setIsOpen(false)}
@@ -72,6 +79,6 @@ export const Activities = () => {
 				currentIndex={imageIndex}
 				setCurrentIndex={setImageIndex}
 			/>
-		</section>
+		</>
 	);
 };
