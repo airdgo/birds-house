0eca89c747bcb8bcdf8bd393089c64d603ba2459
airdog <vladdragoi1@gmail.com>
2023-05-07 15:37:02 +0300

    style: remove livingroom images

--- src/constants/livingroomImages.js
+++ src/constants/livingroomImages.js
@@ -2,11 +2,7 @@ import livingroom1 from "../assets/rooms/livingroom-1.jpeg";
 import livingroom2 from "../assets/rooms/livingroom-2.jpeg";
 import livingroom3 from "../assets/rooms/livingroom-3.jpeg";
 import livingroom4 from "../assets/rooms/livingroom-4.jpeg";
-import livingroom5 from "../assets/rooms/livingroom-5.jpeg";
-import livingroom6 from "../assets/rooms/livingroom-6.jpeg";
-import livingroom7 from "../assets/rooms/livingroom-7.jpeg";
-import livingroom8 from "../assets/rooms/livingroom-8.jpeg";
-import livingroom9 from "../assets/rooms/livingroom-9.jpeg";
+import livingroom5 from "../assets/rooms/livingroom-5.jpg";
 
 export const livingroomImages = [
 	{
@@ -20,8 +16,4 @@ export const livingroomImages = [
 	{ id: 19, src: livingroom3, alt: "livingroom" },
 	{ id: 20, src: livingroom4, alt: "livingroom" },
 	{ id: 21, src: livingroom5, alt: "livingroom" },
-	{ id: 22, src: livingroom6, alt: "livingroom" },
-	{ id: 23, src: livingroom7, alt: "livingroom" },
-	{ id: 24, src: livingroom8, alt: "livingroom" },
-	{ id: 25, src: livingroom9, alt: "livingroom" },
 ];
