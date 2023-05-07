23732ee503ceeb8bf206b30b942aca8fdd74506f
airdog <vladdragoi1@gmail.com>
2023-05-07 14:15:12 +0300

    style: add paragliding images

--- src/constants/paraglidingImages.js
+++ src/constants/paraglidingImages.js
@@ -1,6 +1,10 @@
+import fly0 from "../assets/activities/fly-0.jpeg";
 import fly1 from "../assets/activities/fly-1.jpg";
+import fly2 from "../assets/activities/fly-2.jpg";
 
 const paraglidingImages = [
-	{ index: 17, src: fly1, alt: "paragliders", isVisible: true },
+	{ index: 17, src: fly0, alt: "paragliders", isVisible: true },
+	{ index: 18, src: fly1, alt: "paragliders" },
+	{ index: 19, src: fly2, alt: "paragliders" },
 ];
 export default paraglidingImages;
